ca424695ae130016f79b3710afc5adf4b5f9b8d1
Sergey Kurdin <sergey@ski.com>
2018-06-04 15:38:31 -0400

    removed __fixtures__

--- __fixtures__/component.es6.js
+++
@@ -1,7 +0,0 @@
-import { Component } from 'inferno';
-
-export default class FixtureComponent extends React.Component {
-  render() {
-    return <div>fixture2</div>;
-  }
-}
--- __fixtures__/component.js
+++
@@ -1,5 +0,0 @@
-import { Component } from 'inferno';
-
-module.exports = function Component() {
-  return <div>fixture1</div>;
-};
